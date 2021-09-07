c10b126244b5035bade0202e2f35dc9c0d64eece
Horolsky <horolsky89@outlook.com>
2021-09-07 19:16:23 +0300

    deque: mod indexation optimised

--- deque.hpp
+++ deque.hpp
@@ -3,14 +3,18 @@
 
 typedef unsigned long int size_t;
 
+template <int N>
+inline int mod(int x)
+{
+    return N & (N - 1) ? x % N : x & (N-1);
+}
+
 //fixed size deque
 template <class T, size_t N>
 class deque
 {    
 public:
     typedef T value_type;
-    // deque() { m_data = new T[N]; }
-    // ~deque() { delete[] m_data; }
 
     size_t capacity() {return N; }
     bool empty() const { return m_size == 0; }
@@ -20,35 +24,35 @@ public:
     {
         if (m_size == N) throw std::overflow_error("deque overflow");
         m_data[m_head] = value;
-        m_head = (m_head + 1) % N;
+        m_head = mod<N>(m_head + 1);
         m_size += 1;
     }
     void push_back(const T &value)
     {
         if (m_size == N) throw std::overflow_error("deque overflow");
-        m_data[(N+m_head-(m_size++)-1)%N] = value;
+        m_data[mod<N>(N+m_head-(m_size++)-1)] = value;
     }
     T pop_front()
     {
         if (m_size == 0) throw std::underflow_error("deque underflow");
         m_size -= 1;
-        return m_data[m_head = (m_head+N-1)%N];
+        return m_data[m_head = mod<N>(m_head+N-1)];
     }
     T pop_back()
     {
         if (m_size == 0) throw std::underflow_error("deque underflow");
-        return m_data[(m_head+N-(m_size--))%N];
+        return m_data[mod<N>(m_head+N-(m_size--))];
     }
     T operator[](size_t i) const
     {
         if (i >= m_size) throw std::overflow_error("deque index overflow");
-        return m_data[(m_head+N-i-1)%N];
+        return m_data[mod<N>(m_head+N-i-1)];
     }
     void print() const
     {
         for (int i = 0 ; i < m_size; ++i)
         {
-            std::cout << m_data[(m_head+N-i-1)%N] << ", ";
+            std::cout << m_data[mod<N>(m_head+N-i-1)] << ", ";
         }
         std::cout << "\n";
     }
@@ -62,6 +66,7 @@ public:
     }
     int _hd() const { return m_head; }
 private:
+    
     T m_data[N];
     int m_head{0};
     int m_size{0};
